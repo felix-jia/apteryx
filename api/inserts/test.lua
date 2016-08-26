d4ac0adb5bba643d905ca00118ead34b298fd684
Felix Jia <felix.jia@alliedtelesis.co.nz>
2016-08-26 15:53:26 +1200

    the test api for testing

---
+++ api/inserts/test.lua
@@ -0,0 +1,4 @@
+apteryx_insert("/test/debug",{["disable"]="0",["enable"]="1"},"0")
+apteryx_insert("/test/list/*/name",nil,nil)
+apteryx_insert("/test/list/*/type",{["big"]="1",["little"]="2"},"1")
+apteryx_insert("/test/list/*/sublist/*/i-d",nil,nil)
